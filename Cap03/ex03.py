969f7c7352acf437d90ae3831b3ba1e4c4a728c8
Rafael Rodrigues Gebra <111754401+rafaelgebra@users.noreply.github.com>
2023-12-01 17:26:16 -0300

    Updade_Caps

--- Cap03/ex03.py
+++ Cap03/ex03.py
@@ -13,4 +13,15 @@ INICIO
         
 FIM
 """
+lista = [17,3,2,5,6,4,7]
 
+
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+print(bubble_sort(lista))
