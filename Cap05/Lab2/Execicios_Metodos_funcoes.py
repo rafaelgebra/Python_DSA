116b6167cbe9997c718f237fe66763a7dcace9de
Rafael Rodrigues Gebra <111754401+rafaelgebra@users.noreply.github.com>
2023-12-19 14:05:41 -0300

    Updade

---
+++ Cap05/Lab2/Execicios_Metodos_funcoes.py
@@ -0,0 +1,11 @@
+from platform import python_version
+
+
+def titulo(msg):
+    print()
+    print(f'-- {msg} --')
+    print()
+
+titulo(f'A versão que estamos trabalhando é {python_version()}')
+
+titulo('Exercícios - Métodos e Funções (Solução)')
--- Cap05/Lab2/calculadora_v1.py
+++ Cap05/Lab2/calculadora_v1.py
@@ -5,4 +5,31 @@
 
 print("\n******************* Calculadora em Python *******************")
 
+print('Selecione o número da operação desejada:\n')
+print('1 - Soma')
+print('2 - Subtação')
+print('3 - Multiplocação')
+print('4 - Divisão\n')
+opcao = int(input('Digite sua opção [1, 2, 3, 4]: '))
 
+
+if opcao not in (1, 2, 3, 4):
+    print(('Opção inválida.... '))
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+
+if opcao == 1:
+    total = n1 + n2
+    print(f'{n1} + {n2} = {total}')
+
+elif opcao == 2:
+    total = n1 -n2
+    print(f'{n1} + {n2} = {total}')
+
+elif opcao == 3:
+    total = n1 * n2
+    print(f'{n1} * {n2} = {total}')
+
+elif opcao == 4:
+    total = n1 / n2
+    print(f'{n1} / {n2} = {total}')
