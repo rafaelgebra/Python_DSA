b021d1b2b9e368367337a20d3f02f2107df86108
Rafael Rodrigues Gebra <111754401+rafaelgebra@users.noreply.github.com>
2023-12-25 11:30:54 -0300

    Update

--- teoria/6-Area_teste.py
+++ teoria/6-Area_teste.py
@@ -3,7 +3,7 @@ from time import sleep
 
 def titulo(msg):
     print()
-    print(f'--- {msg} ---', flush=True)
+    print(f'--- {msg.upper()} ---', flush=True)
     #sleep(0.3)
     print()
 
@@ -11,30 +11,51 @@ def titulo(msg):
 def lento(msg):# Lento para frase em str
     for letra in msg:
         print(letra, end='', flush=True)
-        sleep(0.009)
+        sleep(0.001)
     print('\n')
 
 
-print()
-lento('Agora vamos abrir o arquivo para gravação')
-lento('Lembrando que o modulo open() é usado para abrir o arquivo')
-arq2 = open('Cap06/arquivos/arquivo2.txt', 'w')
-print('O w = write. Significa escrita')
-lento('Quando se utiliza o modo "w" tem que ter muita atenção, porque se não encontrar o arquivo solicitado vai ser criado o arquivo se o arquivo já existir o modo "w" vai sobrescrever')
-#print(arq2.read())# se o arquivo foi aberto apenas para escrita 'w = write' o não tem como ler 'r = read'
-print()
-lento('Para gravar no arquivo usar o metodo write()')
-arq2.write('Aprendendo a programar em Python. E a metodologia de ensino da Data Science Academy facilita o aprendizado.')
-arq2.close()
-arq2 = open('Cap06/arquivos/arquivo2.txt', 'r')
-print(arq2.read())
-print()
+titulo('Abrindo Dataset em linha Única')
 
-lento('Agora vamos abrir o arquivo no metodo "a = append" acrescentar')
-arq2 = open('Cap06/arquivos/arquivo2.txt', 'a')
-arq2.write('\nE a metodologia de ensino da Data Science Academy facilita o aprendizado')
-arq2.close()
-arq2 = open('Cap06/arquivos/arquivo2.txt', 'r')
-print(arq2.read())
-arq2.seek(0,0)
-print(arq2.read())
+f = open('Cap06/arquivos/salarios.csv', 'r')
+data = f.read()
+rows = data.split('\n')
+#print(rows)
+
+titulo('Dividindo Um Arquivo em colunas')
+
+f = open('Cap06/arquivos/salarios.csv', 'r')
+data = f.read()
+rows = data.split('\n')
+full_data = []
+for row in rows:
+    split_row = row.split(',')
+    full_data.append(split_row)
+#print(full_data)
+
+titulo('Contando as linha de um arquivo')
+f = open('Cap06/arquivos/salarios.csv', 'r')
+data = f.read()
+rows = data.split('\n')
+full_data = []
+for row in rows:
+    split_row = row.split(',')
+    full_data.append(split_row)
+count = 0
+for row in full_data:
+    count += 1
+print(f'O arquivo "salarios.csv" tem {count} linhas')
+
+titulo('Contando o número de colunas de um arquivo')
+f = open('Cap06/arquivos/salarios.csv', 'r')
+data = f.read()
+rows = data.split('\n')
+full_data = []
+for row in rows:
+    split_row = row.split(',')
+    full_data.append(split_row)
+    first_row = full_data[0]
+count1 = 0
+for calumn in first_row:
+    count1 += 1
+print(count1)
