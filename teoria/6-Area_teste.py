7cb0f5f23fd4b35f7cd3c639ef30691797609580
Rafael Rodrigues Gebra <111754401+rafaelgebra@users.noreply.github.com>
2023-12-28 17:43:28 -0300

    Update

--- teoria/6-Area_teste.py
+++ teoria/6-Area_teste.py
@@ -19,3 +19,8 @@ def lento(msg):# Lento para frase em str
 
 lento('CSV (Comma-Separated Values "VALORES SEPARADOS POR VIRGULA") é um formato de arquivo que armazena dados tabulares em formato de testo plano. Cada linha do arquivo CSV representa uma linha da tabela e as colunas são separadas por vírgulas. É amplamente utilizado para exportar e importar dados em diferentes aplicações, como planilhas e banco de dados. CSV é uma opção simples e universal para compartilhar dados, pois pode ser aberto e editado com muitos aplicativos diferentes, incluindo programas de planilhas e editor de texto.')
 
+with open('Cap06/arquivos/numeros.cvs', 'w') as arquivo8: # Aqui esta fazendo a abertura do arquivo8 e mostrando o caminha com  open('Cap06/arquivos/numeros.cvs', 'w')
+    write = csv.write(arquivo8) #Com o write = csv.write(arquivo*) é criado o objeto de gravação
+    write.writerow(('nota1', 'nota2', 'nota3'))
+    write.writerow((63, 87, 92))
+    wri
