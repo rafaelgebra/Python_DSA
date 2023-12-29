c1472520290d5fcc5c0ed12836b977a0625976b1
Rafael Rodrigues Gebra <111754401+rafaelgebra@users.noreply.github.com>
2023-12-29 11:40:21 -0300

    update

--- teoria/5-Teoria.py
+++ teoria/5-Teoria.py
@@ -3,6 +3,8 @@ from time import sleep
 import pandas as pd
 import os
 import csv
+import json
+from urllib.request import urlopen
 
 def titulo(msg):
     print()
@@ -438,3 +440,58 @@ arquivo7 = open('Cap06/arquivos/cientista.txt', 'r')#Faz novamente a apertura do
 conteudo1 = arquivo7.read()#Grava em uma variável usando o método leitura read()
 arquivo7.close()#fecha o arquivo com cloase()
 print(conteudo1)# imprime o conteúdo da variável 
+
+titulo('Manipulação de arquivos CSV - (Comma-Separated Values "VALORES SEPARADOS POR VIRGULA")')
+
+lento('CSV (Comma-Separated Values "VALORES SEPARADOS POR VIRGULA") é um formato de arquivo que armazena dados tabulares em formato de testo plano. Cada linha do arquivo CSV representa uma linha da tabela e as colunas são separadas por vírgulas. É amplamente utilizado para exportar e importar dados em diferentes aplicações, como planilhas e banco de dados. CSV é uma opção simples e universal para compartilhar dados, pois pode ser aberto e editado com muitos aplicativos diferentes, incluindo programas de planilhas e editor de texto.')
+
+with open('Cap06/arquivos/numeros.cvs', 'w') as arquivo8: # Aqui esta fazendo a abertura do arquivo8 e mostrando o caminha com  open('Cap06/arquivos/numeros.cvs', 'w')
+    write = csv.writer(arquivo8) #Com o write = csv.write(arquivo*) é criado o objeto de gravação
+    write.writerow(('nota1', 'nota2', 'nota3')) #Gravar arquivo linha a linha
+    write.writerow((63, 87, 92))
+    write.writerow((61, 79, 76))
+    write.writerow((72, 64, 91))
+with open('Cap06/arquivos/numeros.cvs', 'r', encoding='utf8', newline='\r\n') as arquivo8:
+    leitor = csv.reader(arquivo8) #Objeto de leitura # com o método reader() é lido cada linha e gera uma lista 
+    for x in leitor: #Loop - vai percorrem o objeto = cada linha do objeto
+        print(x)
+with open('Cap06/arquivos/numeros.cvs', 'r') as arquivo8:
+    leitor = csv.reader(arquivo8)
+    dados = list(leitor)
+print(dados)
+for linha in dados[1]:
+    print(linha)
+
+
+titulo('Manipulando arquivo JSON(Java Script Object Notation)')
+lento('JSON (JavaScript Object Notation) é um  formato de dados de text simples e leve que é utilizado para transmitir informações em aplicações WEB. É baseado em uma estrutura de objetos JavaScript e usa pares de chave-valor para representar dados. JSON é facilmente lidos e escritos por máquinas e é amplamente ultilzado com formatos de intercâmbio de dados em aplicações web modernas\n')
+dict_guido = {'nome': 'Guido van Rossum',
+              'liguagem': 'Python',
+              'similar': ['c', 'Modula-3', 'lisp'],
+              'users': 1000000}
+for k, v in dict_guido.items():
+    print(k, v)
+print()
+
+print(f'Aqui é feito a impressão usando o pacote de importação "JSON" com o método função json.dumps(dict_guido), onde json.dumps é a função onde converte o dicionario Python em formato "JSON" e (dict_guido) é o parametro "dicionario Python".{json.dumps(dict_guido)}\n')
+
+with open('Cap06/arquivos/dados.json', 'w') as arquivo9: # aqui é feita a "gravação/criação".
+    arquivo9.write(json.dumps(dict_guido)) #Novamente (json.dumps(disct_guido)) é a converção do dicionario python para o formato "JSON". O método write é para gravar/criar na variável arquivo9.
+with open('Cap06/arquivos/dados.json', 'r') as arquivo10:# Aqui é aberto novamente o arquivo, só que para leitura agora.
+    texto = arquivo10.read() #Com o método read(), é usado para gravar o conteúdo do arquivo10 em uma variável para impressão
+    dados = json.loads(texto)# usando o método "loads = carregar". Aqui esta "carregando\convertendo" o conteúdo da variável texto na variável dados.
+print(f'Se tentar imprimir o conteúdo da variável arquivo10 sem converte para variável python da o sequinte erro: {arquivo10}')
+print()
+print(dados)
+print(dados['nome'])
+
+titulo('Estraçã de arquivo da WEB')
+
+response = urlopen('http://vimeo.com/api/v2/video/57733101.json').read().decode('utf8')
+dados = json.loads(response)[0]
+print(dados)
+print()
+print('titulo: ', dados['title'] )
+print('URL: ', dados['url'])
+print('Duração: ', dados['duration'],'min')
+print('Número de Visualizações: ', dados['stats_number_of_plays'])
--- teoria/6-Area_teste.py
+++ teoria/6-Area_teste.py
@@ -1,9 +1,11 @@
-from encodings import utf_8
-from operator import length_hint
+from platform import python_version
 from time import sleep
+from urllib import response 
 import pandas as pd
 import os
 import csv
+import json
+from urllib.request import urlopen
 
 def titulo(msg):
     print()
@@ -18,27 +20,18 @@ def lento(msg):# Lento para frase em str
         sleep(0.003)
     print('\n')
 
-titulo('Manipulação de arquivos CSV - (Comma-Separated Values "VALORES SEPARADOS POR VIRGULA")')
+response = urlopen('https://compras.dados.gov.br/licitacoes/doc/licitacao/02000105001802012.json').read().decode('utf8')
+dados = json.loads(response)
 
-lento('CSV (Comma-Separated Values "VALORES SEPARADOS POR VIRGULA") é um formato de arquivo que armazena dados tabulares em formato de testo plano. Cada linha do arquivo CSV representa uma linha da tabela e as colunas são separadas por vírgulas. É amplamente utilizado para exportar e importar dados em diferentes aplicações, como planilhas e banco de dados. CSV é uma opção simples e universal para compartilhar dados, pois pode ser aberto e editado com muitos aplicativos diferentes, incluindo programas de planilhas e editor de texto.')
+arquivo_fonte = 'Cap06/arquivos/pro.json'
+arquivo_destino = 'Cap06/arquivos/dados1.json'
+with open(arquivo_fonte, 'r') as infile:
+    text = infile.read()
+    with open(arquivo_destino, 'w') as outfiles:
+        outfiles.write(text)
+with open('Cap06/arquivos/dados1.json') as arquivo1:
+    texto = arquivo1.read()
+    dados = json.loads(texto)
+print('modalidade: ', dados['modalidade'])
+print('situacao_aviso: ', dados['situacao_aviso'])
 
-with open('Cap06/arquivos/numeros.cvs', 'w') as arquivo8: # Aqui esta fazendo a abertura do arquivo8 e mostrando o caminha com  open('Cap06/arquivos/numeros.cvs', 'w')
-    write = csv.writer(arquivo8) #Com o write = csv.write(arquivo*) é criado o objeto de gravação
-    write.writerow(('nota1', 'nota2', 'nota3')) #Gravar arquivo linha a linha
-    write.writerow((63, 87, 92))
-    write.writerow((61, 79, 76))
-    write.writerow((72, 64, 91))
-with open('Cap06/arquivos/numeros.cvs', 'r', encoding='utf8', newline='\r\n') as arquivo8:
-    leitor = csv.reader(arquivo8) #Objeto de leitura # com o método reader() é lido cada linha e gera uma lista 
-    for x in leitor: #Loop - vai percorrem o objeto = cada linha do objeto
-        print(x)
-with open('Cap06/arquivos/numeros.cvs', 'r') as arquivo8:
-    leitor = csv.reader(arquivo8)
-    dados = list(leitor)
-print(dados)
-for linha in dados[1]:
-    print(linha)
-
-titulo('Manipulando arquivo JSON(Java Script Object Notation)')
-lento('JSON (JavaScript Object Notation) é um  formato de dados de text simples e leve que é utilizado para transmitir informações em aplicações WEB. É baseado em uma estrutura de objetos JavaScript e usa pares de chave-valor para representar dados. JSON é facilmente lidos e escritos por máquinas e é amplamente ultilzado com formatos de intercâmbio de dados em aplicações web modernas')
-dict = {}
