aa5da846508bc5f7870cef9d52ea0d09a6288b3c
Rafael Rodrigues Gebra <rafaelrodriguesgebra@gmail.com>
2024-07-25 19:04:46 -0300

    Update_projeto_jogo_rafael_v2_final

--- Cap07/Jogo_forca_rafael/projeto_jogo_rafael_v2.py
+++ Cap07/Jogo_forca_rafael/projeto_jogo_rafael_v2.py
@@ -20,6 +20,96 @@ def limpar_tela():
         _ = system ("clear") # Se o sistema operacional dor Mac ou Linux, com o comando "clear" é feito a limpesa da tela.
         # O underline é a variavél de retorno da função system, porém não "quero" que tenha algum retorno, então é jogado para essa variavél sem uso.
 
+# Função que desenha a forca na tela.
+def display_hangman(chances):
+    # Lista de estágios da farca, é feito literalmente dentro de uma lista com caracteres.
+    stages = [ # Estágio 6 (final)
+                """
+                    ---------
+                    |       |
+                    |       O
+                    |      \\|/
+                    |       |
+                    |      / \\
+                    |
+                    |_________
+                    
+                """,
+                # Estágio 5
+                """
+                    ---------
+                    |       |
+                    |       O
+                    |      \\|/
+                    |       |
+                    |      / 
+                    |
+                    |_________
+                    
+                """,
+                # Estágio 4
+                """
+                    ---------
+                    |       |
+                    |       O
+                    |      \\|/
+                    |       |
+                    |
+                    |
+                    |_________
+                    
+                """,
+                # Estágio 3
+                """
+                    ---------
+                    |       |
+                    |       O
+                    |      \\|
+                    |       |
+                    |
+                    |
+                    |_________
+                    
+                """,
+                # Estágio 2
+                """
+                    ---------
+                    |       |
+                    |       O
+                    |       |
+                    |       |
+                    |
+                    |
+                    |_________
+                    
+                """,
+                # Estágio 1
+                """
+                    ---------
+                    |       |
+                    |       O
+                    |
+                    |
+                    |
+                    |
+                    |_________
+                    
+                """,
+                # Estágio 0
+                """
+                    ---------
+                    |       |
+                    |
+                    |
+                    |
+                    |
+                    |
+                    |_________
+                    
+                """
+    ]
+    return stages[chances]
+
 #função
 def game():
     # Bloco inicial do game
@@ -33,44 +123,57 @@ def game():
     # Usando a bíblioteca (pacote) python Random consegue usar de forma aleatória a escolha de palavas.
     palavra = random.choice(palavras) # com a função choice (escolher em ingles) do pacote Random faz essa "escolha". 
 
-    # Lista comprehension.
-    letras_descobertas = ["_" for letra in palavra] # Com esse for consegue deixar um underline para cada letra da palavra que precisa ser descoberta
+    #Lista de letras da palavra
+    lista_letras_palavras = [letra for letra in palavra] # Retorna a letra separada da palavra
+
+    # Cria o tabuleiro com caracter "_" Multiplicado pelo comprimento da palavra
+    tabuleiro = ["_"] * len(palavra)
 
     # Número de chances
     chances = 6 # Essa variável limita a quantidade de chances.
 
-    # Lista para as letras erradas. 
-    letras_erradas = [] # Essa lista vai acrescentar a letra erradas, com ela pode ir somando usando o atributo len(), para contar e mostrar para o usuario a quantidade de letra que errou.
+    # Lista para as letras digitadas
+    letras_tentativas = []
 
     # Loop (while), enquanto o número de chances dor maior do que zero vai continuar.
     while chances > 0:
         #print
-        print(" ".join(letras_descobertas)) # O join() faz aqui uma junção entre o espaço em banco e a lista letras_decobertas.
-        print(f"\n\033[;30;42mChance restante:\033[m {chances}")
-        print(f"\033[;30;41mLetras erradas: {" ".join(letras_erradas)}\033[m") # Aqui o join() também faz a função de junção mas nessa caso é a junção do espaço com a variável letra_erradas 
+        print(display_hangman(chances))
+        print("\033[;30;42mPalavra:\033[m", tabuleiro)
+        print("\n") # Um espaço no fim
 
         # Tentativa
         tentativa = input("\nDigite um letra: ").lower()
 
-        # Bloco condicional.
-        if tentativa in palavra: # Se a letra estiver dentro da palavra escolhida, a variável index é criada e recebe 0.
-            index = 0
-            for letra in palavra: # O loop for nesse caso é para checar cada letra dentro da palavra. 
-                if tentativa == letra: # Se a letra digitada em tentativa for igual (==) a letra que foi verificado pelo Loop, entra na proxím
-                    letras_descobertas[index] = letra # se a condição de cima for verdadeira, a lista letras_descobertas recebi no local "index" de aonde foi "encontrada"
-                index += 1 # Esse incremento é dado quando a confição de cima dor verdade
-        else:
-            chances -= 1 # Se a condição estiver errada, é reduzido o número de chances
-            letras_erradas.append(tentativa) # Por letras_erradas ser uma lista é feito um append da letra. podendo mais para frente usar a função len() para contar a quantidade de letras erradas.
-        
-        # Segunda condição.
-        if "_" not in letras_descobertas: # Se os underline não tiver mais na lista de letras_descobertas. o usúario venceu.
-            print(f"\n\033[;30;42mVocê venceu, a palavra era:\033[m {palavra}")
-            break # É dado um break porque o usúario venceu e sai do loop while dando fim ao jogo.
+        if tentativa in letras_tentativas:
+            print("\033[;30;42mVocê já tentou essa letra. escolha outra!\033[m")
+            continue
+
+        # Adiciona as letras/ tentativas na lista de letras_tentaticas.
+        letras_tentativas.append(tentativa)
     
-    # Terceira condição. Analizar se ainda tem underlaine na palavra escolhida.
-    if "_" in letras_descobertas:
-        print(f"\n\033[;30;41mVocê perdeu, a palavra era:\033[m \033[;30;42m{palavra.capitalize()}\033[m")
+        # Condicional
+        if tentativa in lista_letras_palavras:
+            print("\033[;30;42mVocê acertou a letra!\033[m")
+            
+            # Um loop For
+            for indice in range(len(lista_letras_palavras)):
+                # Uma condição.
+                if lista_letras_palavras[indice] == tentativa:
+                    tabuleiro[indice] = tentativa
+        
+            # Se todos os espaças foram preenchidis, o jogo acabou.
+            if "_" not in tabuleiro:
+                print(f"\033[;30;42mVocê venceu! A palavra é:\033[m {palavra}")
+                break
+        else:    
+            print(f"\n\033[;30;41mOps. Essa letra não está na palavra\033[m")
+            chances -= 1
+
+        #Condicional
+    if "_" in tabuleiro:
+        print(f"\n\033[;30;41mVocê perdeu! A palavra era:\033[m {palavra}")
+
 
 # Bloco main. É usado para dizer que esse script é um móduçp Python um programa Python
 
