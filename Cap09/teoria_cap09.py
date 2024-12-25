587af78fcc094ceda2a1c9169ea9f5046088debb
Rafael Rodrigues Gebra <111754401+rafaelgebra@users.noreply.github.com>
2024-12-25 13:49:32 -0300

    Update_theory_Cap09

--- Cap09/teoria_cap09.py
+++ Cap09/teoria_cap09.py
@@ -118,3 +118,110 @@ print()
 print("Fução logspace() no NumPy é usda para criar uma sequência de números igualmente espacados em escala logarítmica dentro de um intervalo especifico. Essa função é amplamente ultilizada em programação científica e matemática para gerar arrays de números para diversos fins, como gráficos, cálculos e simulações.")
 print(dsa.logspace(0, 5, 10))
 # Manipulação de matrizes NumPy - Parte 1 de 2
+print("Criando uma Matriz, Isso simplesmente é uma lista de listas (isso é conhecido como lista aninhada)")
+arr9 = dsa.array([[1, 2, 3,],[4, 5, 6,]])
+print(f"Mostar que é uma ndarray: um array da bíblioteca NumPy. {type(arr9)}")
+print(f"Imprimindo a MATRIZ:\n{arr9}")
+print(f"Com o .shape, mostra qual a dimenção dessa matriz: {arr9.shape}, Duas linhas e Três colunas.")
+print(f"Criando uma matriz de ((2, 3)) apenas com número 1. è criada usando o método .ones().")
+arr10 = dsa.ones((2,3,))
+print(f"Fica assim:\n{arr10}")
+lista = [[13, 18, 22,], [0, 34, 59,], [21, 48, 94,]]
+arr11 = dsa.matrix(lista)
+print(f"Agora vamos criar uma matriz tendo como párametro uma lista de listas.\nO comando é .matrix(). Então para fazer isso tem que usar o NumPy e o método tendo como párametro a lista.\narr11 = dsa.matrix(lista) o resultado é:\n {arr11}")
+print(f"Como de costume verificar o tipo: {type(arr11)}, interessante notar que é uma matriz agora.")
+print(f"Usando o método .shape(), podemos descobrir o formato dessa matriz.\nO formato é: {dsa.shape(arr11)}. O arr11 é uma matriz de duas dimenções, 3 linhas e 3 colunas.")
+print(f"O tipo de dado {arr11.dtype}")
+print(f"Usando um outro atribudo ddo abjeto aar11.\nEsse atributo é o .size.\nO resultado é: {arr11.size}. Size é tamanho em inglês, então isso significa que o tamanho dessa matriz é {arr11.size}")
+print("INDEXAÇÃO COM DUAS DIMENÇÕES da Matriz.")
+print(f"O que muda?\nÉ que agora consegue preencher o número do índice para cada dimenção:\nEx. {arr11[2,1]}. Esse valor significa que retornou o valor da linha 2, da coluna 1")
+print(f"Fazendo o mesmo exemplo só que agora com um range de de linhas arr11[0:2,2].\nO resultado é:\n {arr11[0:2,2]}.\nÉ esse resultado porque limita as linhas do índice 0 e 1, e busca a coluna do índice 2 que é a coluna 2.")
+print(f"Terceiro exemplo de indexação de matriz.\nCom esse tipo de fatiamento aee11[1,] é retornado todas as colunas do índice da linha 1.\nResultado: {arr11[1,]}")
+print("Para fazer alteração de um elemento de uma matriz é só usar o mesmo sistema que faz com lista.")
+print(f"Pode ser feito dessa forma arr11[1,0] = 100.\nAntes.\n{arr11} ")
+arr11[1,0] = 100
+print(f"Depois.\n{arr11}\n")
+print(f"Tem como FORÇAR o tipo de dado que o arry.\nPor exemplo:\nx = dsa.array([1, 2]) # Dessa forma é deixar o proprio interpretador do numpy decidir qual é o tipo de dado.\nOutro exemplo:\ny = dsa.array([1.0, 2.0]) # Novamente o NumPy esta escolhendo o tipo de dado.\nAgora vamos ver como FORÇAR o tipo de dado, é só indicar qual o tipo de dado quer.\nExmplo:\nx = dsa.array([1, 2], dtype = dsa.float64) # Dessa forma esta forçando ser float mesmo que o tipo deja int.")
+x = dsa.array([1, 2])
+y = dsa.array([1.0, 2.0])
+z = dsa.array([1, 2], dtype = dsa.float64)
+print(f"Resultado de y: {x.dtype}\nResultado de y: {y.dtype}\nResultado de z: {z.dtype}")
+print(f"Criando uma nova lista de lista = uma matriz. Com os seguintes dados e array: arr12 = dsa.array([[24, 76, 92, 14], [47, 35, 89, 2]], dtype = float)")
+arr12 = dsa.array([[24, 76, 92, 14], [47, 35, 89, 2]], dtype = float)
+print(f"O resuldado é uma matriz de dados do tipo float conforme FORÇADO pelo comando dtype.float.\n{arr12}")
+print(f"O itemsize de um array numpy é um atributo que retorna o tamanho em bytes de cada elemento em bytes de cada elemento do array. Em outras palavras. o itemsize representa o número de bytes necessários para armazenar cada valor do array numpy.")
+print(f"Vamos usar o atributo .itemsize: {arr12.itemsize}, esse número é o tamanho em bytes de cada elemneto arr12.\nPara somar todo os bytes é usado o attibuto .nbytes: {arr12.nbytes}, esse valor total é a quantidade de bytes que essa matriz esta gastando de memória do computador.")
+print(f"Agora vamos ver a quantidade de dimenções usando o atributo de número de imenções .ndim. o arr12 tem: {arr12.ndim}\n")
+print(f"Manipulando Objetos de 3 e 4 dimensões com NumPy.")
+
+print(f"Criando um array de 3 dimensões.")
+arr_3d = dsa.array([[[1, 2, 3, 4], [5, 6, 7, 8], [9, 10 , 11, 12]],[[13, 14, 15, 16], [17, 18, 19, 20], [21, 22, 23, 24]]])
+facil_visu = """
+    arr_3d = dsa.array([
+    [
+        [1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [9, 10 , 11, 12]
+    ],
+    [
+        [13, 14, 15, 16],
+        [17, 18, 19, 20],
+        [21, 22, 23, 24]
+    ]    
+])
+"""
+print(f"Isso é uma array de 3 dimensões,\narr_3d = dsa.array([[[1, 2, 3, 4], [5, 6, 7, 8], [9, 10 , 11, 12]],[[13, 14, 15, 16], [17, 18, 19, 20], [21, 22, 23, 24]]]).\nPara facilitar a visualisação fica assim.\n{facil_visu}")
+print(f"A quantidade de dimenções é novamente chamada com o atributo .ndim.\nEntão o número de dimensões da arr_3d é: {arr_3d.ndim}\n")
+print(f"Com o atributo .shape podemos conprovar: {arr_3d.shape}.\n EXPLICAÇÃO: O primeiro eixo tem 2 elementos Dentro de um elemento tem 3 linhas por isso o numero 3 no shape. E para cada linha tem 4 colunas.\n")
+print(f"Para filtras as dimenções segue o mesmo principio.\nEx: arr_3d[0, 2, 1] o resultado será 10.\nComprovando usando o código {arr_3d[0, 2, 1]}\n# busca é feita como é em lista por índice.\n")
+print(f"Criando um array de 4 dimensões.")
+arr_4d = dsa.array([[[[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15],[16,17,18,19,20]],[[21,22,23,24,25],[26,27,28,29,30],[31,32,33,34,35],[36,37,38,39,40]],[[41,42,43,44,45],[46,47,48,49,50],[51,52,53,54,55],[56,57,58,59,60]]],[[[61,62,63,64,65],[66,67,68,69,70],[71,72,73,74,75],[76,77,78,79,80]],[[81,82,83,84,85],[86,87,88,89,90],[91,92,93,94,95],[96,97,98,99,100]],[[101,102,103,104,105],[106,107,108,109,110],[111,112,113,114,115],[116,117,118,119,120]]]])
+facil_visu2 = """arr_4d = dsa.array([
+    [
+        [
+            [1,2,3,4,5],
+            [6,7,8,9,10],
+            [11,12,13,14,15],
+            [16,17,18,19,20]
+        ],
+        [
+            [21,22,23,24,25],
+            [26,27,28,29,30],
+            [31,32,33,34,35],
+            [36,37,38,39,40]
+        ],
+        [
+            [41,42,43,44,45],
+            [46,47,48,49,50],
+            [51,52,53,54,55],
+            [56,57,58,59,60]
+        ]
+    ],
+    [
+        [
+            [61,62,63,64,65],
+            [66,67,68,69,70],
+            [71,72,73,74,75],
+            [76,77,78,79,80]
+        ],
+        [
+            [81,82,83,84,85],
+            [86,87,88,89,90],
+            [91,92,93,94,95],
+            [96,97,98,99,100]
+        ],
+        [
+            [101,102,103,104,105],
+            [106,107,108,109,110],
+            [111,112,113,114,115],
+            [116,117,118,119,120]
+        ]
+    ]
+])
+"""
+print(f"A visualização de uma matriz de 4 dimensões é assim arr_4d = dsa.array([[[[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15],[16,17,18,19,20]],[[21,22,23,24,25],[26,27,28,29,30],[31,32,33,34,35],[36,37,38,39,40]],[[41,42,43,44,45],[46,47,48,49,50],[51,52,53,54,55],[56,57,58,59,60]]],[[[61,62,63,64,65],[66,67,68,69,70],[71,72,73,74,75],[76,77,78,79,80]],[[81,82,83,84,85],[86,87,88,89,90],[91,92,93,94,95],[96,97,98,99,100]],[[101,102,103,104,105],[106,107,108,109,110],[111,112,113,114,115],[116,117,118,119,120]]]]).\nPara facilitar a visualização:\n{facil_visu2}")
+print(f"Novamente com atributo .ndim, para visualizar a quantidade de dimesões: {arr_4d.ndim}")
+print(f"Visualizando o shape. {arr_4d.shape}.\nO número 2 é o primeiro eixo da dimensão, isso significa que tem 2 grupos.\nDentro de qualquer um desses grupos tem mais 3 grupos de elementros dentro do shape.\nDentro de qualquer um desses 4 grupos tem a linhas no caso tem 4 linhas e é composta de 5 colunas.")
+print("Aplicando uma filtragem do array (arr_4d), usando o fatiamento - arr_4d[0,2,1]")
+print(f"O resultado é a linha com os seguintes dados:[46,47,48,49,50]\nComprovando com o código:{arr_4d[0,2,1]}")
+print(f"Usando a mesma indexação, trazer as infirmações de uma determinada coluna. arr_4d[0,2,1,4]. O resultado é 50. Comprovando com o código: {arr_4d[0,2,1,4]}")
