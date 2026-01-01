828d2eb51c48fe380f741573a8a80b1faf99c819
Rafael Rodrigues Gebra <111754401+rafaelgebra@users.noreply.github.com>
2026-01-01 12:05:24 -0300

    Create_pratica_Cap04

---
+++ Fundamentos_Python_IA/Cap04/Pretica_Cap04.py
@@ -0,0 +1,13 @@
+nome_aluno = "TROXXA"
+nota1 = 6
+nota2 = 7
+
+media = (nota1 + nota2) / 2
+
+print(f"A media do aluno {nome_aluno} é: {media}")
+
+if media >= 6:
+    print(f"O aluno {nome_aluno} foi aprovado!")
+
+else:
+    print(f"O aluno {nome_aluno} foi reprovado!")
