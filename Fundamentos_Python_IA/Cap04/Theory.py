e56460b840a71ae8f8f13c496958e2f6ec45ab45
Rafael Rodrigues Gebra <111754401+rafaelgebra@users.noreply.github.com>
2026-01-02 19:10:50 -0300

    Update_name_Pratica/Create_file_Theory

---
+++ Fundamentos_Python_IA/Cap04/Theory.py
@@ -0,0 +1,19 @@
+#Tipos Primitivos em Python.
+
+# Integer (inteiro)
+numero_inteiro = 100
+print(f"Valor{numero_inteiro}, tipo {type(numero_inteiro)}")
+
+# Floa (Ponto Flutuante)
+numero_decimal = 19.99
+print(f"Valor {numero_decimal}, Tipo {type(numero_decimal)}")
+
+#String (Texto)
+texto = "Python é incrivel!!!"
+print(f"Valor {texto}, tipo: {type(texto)}")
+
+#Boolean (Booleano)
+verdadeiro = True
+falso = False
+print(f"valor: {verdadeiro}, Tipo: {type(verdadeiro)}")
+print(f"valor: {falso}, Tipo: {type(falso)}")
